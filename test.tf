5d854de29aea0028fc468748f8a1de6ad0a85b89
Ubuntu <azureuser@Jenkins.e5rilnyjlojuliobecpthkemmb.jx.internal.cloudapp.net>
2019-04-05 15:13:05 +0000

    test.tf update

--- test.tf
+++ test.tf
@@ -1,5 +1,8 @@
 provider "azurerm" {
-  
+  subscription_id = "${var.subscription_id}"
+  client_id = "${var.service_principal_id}"
+  client_secret = "${var.service_principal_key}"
+  tenant_id = "${var.tenant_id}"
 
 }
 resource "azurerm_resource_group" "rg" {
